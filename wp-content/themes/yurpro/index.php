262d61da44b3b564f38408d61d65b4334582a9aa
kazumichi kuroda <kazumichi96@gmail.com>
2017-05-19 18:42:54 +0900

    YURPROテーマ作成開始

---
+++ wp-content/themes/yurpro/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>YURPRO</title>
+        <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.9.1/build/cssreset/cssreset-min.css">
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        Hello Theme!
+    </body>
+</html>
