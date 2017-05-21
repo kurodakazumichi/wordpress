10bbec9d32f8040fa85d6a2b791ada72b727505e
kazumichi kuroda <kazumichi96@gmail.com>
2017-05-22 02:30:53 +0900

    wp-footer()の呼び出しが抜けてた。

--- wp-content/themes/yurpro/footer.php
+++ wp-content/themes/yurpro/footer.php
@@ -2,5 +2,6 @@
                 <p>©Copyright Since 2017 <?php bloginfo("name"); ?> All Right Reserved.</p>
             </footer>
         </div>
+        <?php wp_footer(); ?>
     </body>
 </html>
